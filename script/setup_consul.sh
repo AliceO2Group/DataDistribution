c5298143c3efae7c5793eb57925fc5e40dd804c8
Gvozden Neskovic <neskovic@compeng.uni-frankfurt.de>
2019-10-18 15:53:46 +0000

    Bump Consul Docker image

--- script/setup_consul.sh
+++ script/setup_consul.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -u
 
-docker pull consul:1.6.0
+docker pull consul:1.6.1
 
 sudo firewall-cmd --zone=public --add-port=8500/tcp
 sudo firewall-cmd --zone=public --add-port=8600/udp
+
+docker rm consul-datadist
 
 sudo docker run \
         -p 8500:8500 \
         -p 8600:8600/udp \
         --name=consul-datadist \
-        consul:1.6.0 agent -server -ui -bootstrap -client=0.0.0.0
+        consul:1.6.1 agent -server -ui -bootstrap -client=0.0.0.0
