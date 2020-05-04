a305534157c141045d3eb6e22f70d484ca4c35b3
neskovic@gmail.com <neskovic@gmail.com>
2020-05-04 07:45:26 +0200

    region: implement shm data region for file inject

--- src/common/include/MemoryUtils.h
+++ src/common/include/MemoryUtils.h
@@ -16,6 +16,7 @@
 #define DATADIST_MEMORY_UTILS_H_
 
 #include <boost/container/pmr/memory_resource.hpp>
+#include <boost/filesystem.hpp>
 
 #include <fairmq/FairMQDevice.h>
 #include <fairmq/FairMQChannel.h>
@@ -28,6 +29,10 @@
 #include <thread>
 #include <chrono>
 
+#include <sys/mman.h>
+#include <cstdlib>
+#include <unistd.h>
+
 class DataHeader;
 class FairMQUnmanagedRegion;
 
@@ -36,24 +41,75 @@ namespace o2
 namespace DataDistribution
 {
 
+static constexpr const char *ENV_SHM_PATH = "DATADIST_SHM_PATH";
+
 class FMQUnsynchronizedPoolMemoryResource : public boost::container::pmr::memory_resource
 {
 
 public:
   FMQUnsynchronizedPoolMemoryResource() = delete;
 
-  FMQUnsynchronizedPoolMemoryResource(FairMQChannel &pChan,
-                                      const std::size_t pSize, const std::size_t pObjSize)
-  : mChan(pChan),
+  FMQUnsynchronizedPoolMemoryResource(std::string pSegmentName, FairMQChannel &pChan,
+                                      const std::size_t pSize, const std::size_t pObjSize,
+                                      std::uint64_t pRegionFlags = 0)
+  : mSegmentName(pSegmentName),
+    mChan(pChan),
     mObjectSize(pObjSize),
     mAlignedSize((pObjSize + sizeof(max_align_t) - 1) / sizeof(max_align_t) * sizeof(max_align_t))
   {
+    int lMapFlags = 0;
+    std::string lSegmentRoot = "";
 
-    mRegion = pChan.NewUnmanagedRegion(pSize,
+    // don't reserve swap space and try to lock the region
+#if defined(MAP_NORESERVE) && defined(MAP_LOCKED)
+    lMapFlags = MAP_NORESERVE | MAP_LOCKED;
+#endif
+
+    // try to use different file mapping (hugetlbfs)
+    const auto lHugetlbfsPath = std::getenv(ENV_SHM_PATH);
+    if (lHugetlbfsPath) {
+      do {
+        // make sure directory exists
+        namespace bfs = boost::filesystem;
+        bfs::path lDirPath(lHugetlbfsPath);
+        if (!bfs::is_directory(lDirPath)) {
+          DDLOGF(fair::Severity::ERROR, "Hugetlbfs mountpoint does not exist. Not using huge pages. {}={}",
+            ENV_SHM_PATH, lHugetlbfsPath);
+          break;
+        }
+
+        // check if the hugetlbs is writeable
+        if (0 != access(lHugetlbfsPath, W_OK)) {
+          DDLOGF(fair::Severity::ERROR, "Hugetlbfs mountpoint is not writeable. "
+            "Make sure the permissions are properly set. {}={}", ENV_SHM_PATH, lHugetlbfsPath);
+          break;
+        }
+
+        lSegmentRoot = lHugetlbfsPath;
+        lSegmentRoot += bfs::path::preferred_separator;
+      } while (false);
+    }
+
+
+    DDLOGF(fair::Severity::INFO, "Creating new UnmanagedRegion name={} path={} size={} channel={}",
+      mSegmentName, lSegmentRoot, pSize, pChan.GetName());
+
+    mRegion = pChan.Transport()->CreateUnmanagedRegion(
+      pSize,
+      pRegionFlags,
       [this](void* pRelData, size_t pRelSize, void* /* hint */) {
-      // callback to be called when message buffers no longer needed by transport
-      reclaimSHMMessage(pRelData, pRelSize);
-    });
+        // callback to be called when message buffers no longer needed by transport
+        reclaimSHMMessage(pRelData, pRelSize);
+      },
+      lSegmentRoot.c_str(),
+      lMapFlags
+    );
+
+    if (!mRegion) {
+      DDLOGF(fair::Severity::FATAL, "Creation of new memory region failed. name={} size={} path={} channel={}",
+      mSegmentName, pSize, lSegmentRoot, pChan.GetName());
+      throw std::bad_alloc();
+    }
 
     // prepare header pointers
     unsigned char* lObj = static_cast<unsigned char*>(mRegion->GetData());
@@ -95,31 +151,34 @@ public:
   inline auto allocator() { return boost::container::pmr::polymorphic_allocator<o2::byte>(this); }
 
 protected:
-  virtual void* do_allocate(std::size_t , std::size_t) override
+  virtual void* do_allocate(std::size_t , std::size_t) override final
   {
     unsigned long lAllocAttempt= 0;
     auto lRet = try_alloc();
     // we cannot fail! report problem if failing to allocate block often
     while (!lRet) {
-      using namespace std::chrono_literals;
-
-      if (++lAllocAttempt % 1000 == 0) {
-        DDLOG(fair::Severity::ERROR) << "FMQUnsynchronizedPoolMemoryResource: failing to get free block of "
-                     << mObjectSize << " B, total region size: " << mRegion->GetSize() << " B";
-        DDLOG(fair::Severity::ERROR) << "Downstream components are creating back-pressure!";
-      }
-
-      std::this_thread::sleep_for(1ms);
       // try to reclaim if possible
       try_reclaim();
       // try again
       lRet = try_alloc();
+
+      if (!lRet) {
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(1ms);
+
+        if (++lAllocAttempt % 512 == 0) {
+          DDLOGF(fair::Severity::WARNING,
+            "FMQUnsynchronizedPoolMemoryResource: failing to allocate free block of {} B, total region size: {} B",
+            mObjectSize, mRegion->GetSize());
+          DDLOGF(fair::Severity::WARNING, "Memory segment '{}' too small or large back-pressure.", mSegmentName);
+        }
+      }
     }
 
     return lRet;
   }
 
-  virtual void do_deallocate(void *, std::size_t, std::size_t) override
+  virtual void do_deallocate(void *, std::size_t, std::size_t) override final
   {
     // Objects are only freed through SHM message reclaim callback.
     // This is intentionally noop
@@ -163,6 +222,9 @@ private:
     mReclaimedObjects.push_back(pData);
   }
 
+  /// fields
+  std::string mSegmentName;
+
   FairMQChannel& mChan;
 
   std::unique_ptr<FairMQUnmanagedRegion> mRegion;
@@ -174,6 +236,261 @@ private:
 
   // two step reclaim to avoid lock contention in the allocation path
   std::vector<void*> mReclaimedObjects;
+};
+
+
+
+class RegionAllocatorResource : public boost::container::pmr::memory_resource
+{
+
+public:
+  RegionAllocatorResource() = delete;
+
+  RegionAllocatorResource(std::string pSegmentName, FairMQChannel &pChan,
+                            const std::size_t pSize, std::uint64_t pRegionFlags = 0)
+  : mSegmentName(pSegmentName),
+    mChan(pChan)
+  {
+    int lMapFlags = 0;
+    std::string lSegmentRoot = "";
+
+    // don't reserve swap space and try to lock the region
+#if defined(MAP_NORESERVE) && defined(MAP_LOCKED)
+    lMapFlags = MAP_NORESERVE | MAP_LOCKED;
+#endif
+
+    // try to use different file mapping (hugetlbfs)
+    const auto lHugetlbfsPath = std::getenv(ENV_SHM_PATH);
+    if (lHugetlbfsPath) {
+      do {
+        // make sure directory exists
+        namespace bfs = boost::filesystem;
+        bfs::path lDirPath(lHugetlbfsPath);
+        if (!bfs::is_directory(lDirPath)) {
+          DDLOGF(fair::Severity::ERROR, "Hugetlbfs mountpoint does not exist. Not using huge pages. {}={}",
+            ENV_SHM_PATH, lHugetlbfsPath);
+          break;
+        }
+
+        // check if the hugetlbs is writeable
+        if (0 != access(lHugetlbfsPath, W_OK)) {
+          DDLOGF(fair::Severity::ERROR, "Hugetlbfs mountpoint is not writeable. "
+            "Make sure the permissions are properly set. {}={}", ENV_SHM_PATH, lHugetlbfsPath);
+          break;
+        }
+
+        lSegmentRoot = lHugetlbfsPath;
+        lSegmentRoot += bfs::path::preferred_separator;
+      } while (false);
+    }
+
+    DDLOGF(fair::Severity::INFO, "Creating new UnmanagedRegion name={} path={} size={} channel={}",
+      mSegmentName, lSegmentRoot, pSize, pChan.GetName());
+
+    mRegion = pChan.Transport()->CreateUnmanagedRegion(
+      pSize,
+      pRegionFlags,
+      [this](void* pRelData, size_t pRelSize, void* /* hint */) {
+        // callback to be called when message buffers no longer needed by transport
+        reclaimSHMMessage(pRelData, pRelSize);
+      },
+      lSegmentRoot.c_str(),
+      lMapFlags
+    );
+
+    if (!mRegion) {
+      DDLOGF(fair::Severity::FATAL, "Creation of new memory region failed. name={} size={} path={} channel={}",
+      mSegmentName, pSize, lSegmentRoot, pChan.GetName());
+      throw std::bad_alloc();
+    }
+
+    mStart = static_cast<char*>(mRegion->GetData());
+    mLength = mRegion->GetSize();
+
+    memset(mStart, 0xAA, mLength);
+  }
+
+  inline
+  std::unique_ptr<FairMQMessage> NewFairMQMessage(std::size_t pSize) {
+    auto* lMem = do_allocate(pSize, alignof(std::max_align_t));
+    return mChan.NewMessage(mRegion, lMem, pSize);
+  }
+
+  inline
+  std::unique_ptr<FairMQMessage> NewFairMQMessageFromPtr(void *pPtr, const std::size_t pSize) {
+    assert(pPtr >= static_cast<char*>(mRegion->GetData()));
+    assert(static_cast<char*>(pPtr)+pSize <= static_cast<char*>(mRegion->GetData()) + mRegion->GetSize());
+    return mChan.NewMessage(mRegion, pPtr, pSize);
+  }
+
+  inline
+  auto allocator() { return boost::container::pmr::polymorphic_allocator<o2::byte>(this); }
+
+protected:
+  // NOTE: we align sizes of returned messages, but keep the exact size for allocation
+  //       otherwise the shm messages would be larger than requested
+  static constexpr inline
+  std::size_t align_size_up(const std::size_t pSize) {
+    return (pSize + sAlignment - 1) / sAlignment * sAlignment;
+  }
+
+  virtual void* do_allocate(std::size_t pSize, std::size_t /* pAlign */) override final
+  {
+    unsigned long lAllocAttempt= 0;
+
+    // align up
+    pSize = align_size_up(pSize);
+
+    auto lRet = try_alloc(pSize);
+    // we cannot fail! report problem if failing to allocate block often
+    while (!lRet) {
+      // try to reclaim if possible
+      if (try_reclaim(pSize)) {
+        // try again
+        lRet = try_alloc(pSize);
+      }
+
+      if (!lRet) {
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(1ms);
+
+        if (++lAllocAttempt % 512 == 0) {
+          DDLOGF(fair::Severity::WARNING,
+            "RegionAllocatorResource: failing to allocate free block of {} B, total region size: {} B",
+            pSize, mRegion->GetSize());
+          DDLOGF(fair::Severity::WARNING, "Memory segment '{}' too small or large back-pressure.", mSegmentName);
+        }
+      }
+    }
+
+    return lRet;
+  }
+
+  virtual void do_deallocate(void *pAddr, std::size_t pSize, std::size_t) override final
+  {
+    // Objects are only freed through SHM message reclaim callback.
+    reclaimSHMMessage(pAddr, pSize);
+  }
+
+  virtual bool do_is_equal(const memory_resource &) const noexcept override { return false; }
+
+private:
+  void* try_alloc(const std::size_t pSize) {
+    if (mLength >= pSize) {
+      const auto lObjectPtr = mStart;
+
+      mStart += pSize;
+      mLength -= pSize;
+
+      if (mLength == 0) {
+        mStart = nullptr;
+      }
+
+      return lObjectPtr;
+    }
+
+    return nullptr;
+  }
+
+  bool try_reclaim(const std::size_t pSize) {
+    // First declare any leftover memory as free
+    if (mLength > 0) {
+      assert(mStart != nullptr);
+      reclaimSHMMessage(mStart, mLength);
+      // invalidate the working extent
+      mLength = 0;
+      mStart = nullptr;
+    }
+
+    std::scoped_lock lock(mReclaimLock);
+
+    if (mFrees.empty()) {
+      return false;
+    }
+
+    // find the largest free extent and return it if the size is adequate
+    auto lMaxIter = std::max_element(std::begin(mFrees), std::end(mFrees),
+      [](const auto& l, const auto& r) { return l.second < r.second; });
+
+    // check if the size is adequate
+    if (pSize > lMaxIter->second) {
+      return false;
+    }
+
+    // return the extent
+    mStart = const_cast<char*>(lMaxIter->first);
+    mLength = lMaxIter->second;
+    mFrees.erase(lMaxIter);
+
+    return true;
+  }
+
+  void reclaimSHMMessage(void* pData, size_t pSize)
+  {
+    // align up
+    pSize = align_size_up(pSize);
+
+    const char *lData = reinterpret_cast<const char*>(pData);
+
+    std::scoped_lock lock(mReclaimLock);
+
+    // push object to the free map. Try to merge nodes
+    const auto lIter = mFrees.lower_bound(lData);
+    bool lInserted = false;
+
+    assert(lIter== mFrees.end() || lIter->first != lData);
+
+    // check if we can merge with the previous
+    if (!mFrees.empty() && lIter != mFrees.begin()) {
+      auto lPrev = std::prev(lIter);
+
+      if ((lPrev->first + lPrev->second) == lData) {
+        lPrev->second += pSize;
+        lInserted = true;
+
+        // check if we also can merge with the next (lIter)
+        if (lIter != mFrees.end()) {
+          if ((lPrev->first + lPrev->second) == lIter->first) {
+            lPrev->second += lIter->second;
+            mFrees.erase(lIter);
+          }
+        }
+      }
+    }
+
+    if (!lInserted) {
+      // insert the new range
+      auto lIt = mFrees.emplace_hint(lIter, lData, pSize);
+
+      if (lIt->second != pSize) {
+        DDLOGF(fair::Severity::ERROR, "BUG: RegionAllocatorResource: REPEATED INSERT!!! "
+          " {:p} : {}, original size: {}", lIt->first, pSize, lIt->second);
+      }
+
+      // check if we can merge with the next
+      auto lNextIt = std::next(lIt);
+      if (lNextIt != mFrees.cend()) {
+        if ((lData + pSize) == lNextIt->first) {
+          lIt->second += lNextIt->second;
+          mFrees.erase(lNextIt);
+        }
+      }
+    }
+  }
+
+  /// fields
+  std::string mSegmentName;
+
+  FairMQChannel& mChan;
+  std::unique_ptr<FairMQUnmanagedRegion> mRegion;
+
+  static const constexpr std::size_t sAlignment = alignof(std::max_align_t);
+  char *mStart = nullptr;
+  std::size_t mLength = 0;
+
+  // two step reclaim to avoid lock contention in the allocation path
+  std::mutex mReclaimLock;
+  std::map<const char*, std::size_t> mFrees; // keep all returned blocks
 };
 
 }
