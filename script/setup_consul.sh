8598af862410b64d5298f7944a513678e16c7e9a
neskovic@gmail.com <neskovic@gmail.com>
2020-08-31 15:15:38 +0200

    scripts: Update consul docker image to 1.8

--- script/setup_consul.sh
+++ script/setup_consul.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -u
 
-docker pull consul:1.6.1
+docker pull consul:1.8
 
 sudo firewall-cmd --zone=public --add-port=8500/tcp
 sudo firewall-cmd --zone=public --add-port=8600/udp
@@ -12,4 +12,4 @@ sudo docker run \
         -p 8500:8500 \
         -p 8600:8600/udp \
         --name=consul-datadist \
-        consul:1.6.1 agent -server -ui -bootstrap -client=0.0.0.0
+        consul:1.8
