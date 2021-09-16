ae458eb5495dd39a0207fc73b1feb60bf7383221
Gvozden Neskovic <neskovic@compeng.uni-frankfurt.de>
2021-09-16 18:04:26 +0200

    scripts: bump consul version

--- script/setup_consul.sh
+++ script/setup_consul.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -u
 
-docker pull consul:1.8
+docker pull consul:1.10
 
 sudo firewall-cmd --zone=public --add-port=8500/tcp
 sudo firewall-cmd --zone=public --add-port=8600/udp
@@ -12,4 +12,4 @@ sudo docker run \
         -p 8500:8500 \
         -p 8600:8600/udp \
         --name=consul-datadist \
-        consul:1.8
+        consul:1.10
